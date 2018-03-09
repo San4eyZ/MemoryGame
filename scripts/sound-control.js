c0d12eda2b60cef45e380a258417d1c8fd35708a
Александр <sanya.rogov37@gmail.com>
2018-03-09 17:17:28 +0500

    Initial commit

--- scripts/sound-control.js
+++ scripts/sound-control.js
@@ -8,20 +8,22 @@ const wrong = audioHolder.querySelector('#audio-wrong');
 const success = audioHolder.querySelector('#audio-success');
 const main = audioHolder.querySelector('#audio-main');
 
-sounds.playClick = play.bind(null, click);
-sounds.playTada = play.bind(null, tada);
-sounds.playWrong = play.bind(null, wrong);
-sounds.playSuccess = play.bind(null, success);
-sounds.playMain = () => {
-    setVolume(main, 0.05);
-    play(main);
+sounds.playClick = play.bind(null, click, 0.5);
+sounds.playWrong = play.bind(null, wrong, 0.5);
+sounds.playSuccess = play.bind(null, success, 0.5);
+sounds.playMain = play.bind(null, main, 0.05);
+// По какой-то неведомой мне причине, звуки, проигрываемые при окончании воспроизводятся дважды
+// если проигрывать с помощью play. Потому для финального звука другая функция
+sounds.playTada = () => {
+    if (tada.paused) {
+        tada.play();
+    }
 };
+sounds.play = play;
 
-function setVolume(audio, volume) {
+function play(audio, volume) {
     audio.volume = volume;
-}
 
-function play(audio) {
     audio.pause();
     audio.currentTime = 0;
     audio.play();
